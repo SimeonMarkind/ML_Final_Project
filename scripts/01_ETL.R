a8355f13670a824d082953d638b6c5e3ad027e7a
George Posner <gposner@rstudiopro2.chicagobooth.edu>
2019-02-11 16:16:15 -0600

    include logging. skip csv 14 (revisit later)

--- scripts/01_ETL.R
+++ scripts/01_ETL.R
@@ -1,9 +1,12 @@
-environment<-"AWS"
-#environment<-"UChicago"
+sink(file=paste0("~/ML_Final_Project/logs/01_ETL",format(Sys.time(),"%Y_%m_%d_%H_%M_%S"),".log"))
+
+#environment<-"AWS"
+environment<-"UChicago"
 #environment<-"localhost"
+environment
 
 # Relevant tutorials
-### https://aws.amazon.com/blogs/machine-learning/using-r-with-amazon-sagemaker/
+### AWS: https://aws.amazon.com/blogs/machine-learning/using-r-with-amazon-sagemaker/
 
 
 
@@ -19,11 +22,16 @@ for(y in 2009:2016) {
   for(m in 1:12) {
     if(y<2016 | m <= 6) {
       ncsv<-ncsv+1
-      yyyy<-sprintf("%04d",y)
-      mm<-sprintf("%02d",m)
-      print(paste0("starting ncsv=",ncsv,",yyyy=",yyyy,",mm=",mm))
-      csv[ncsv]<-read_csv(file=paste0("https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_",yyyy,"-",mm,".csv"))
-      print(paste0("finished ncsv=",ncsv,",yyyy=",yyyy,",mm=",mm))
+      if(ncsv != 14) {
+        yyyy<-sprintf("%04d",y)
+        mm<-sprintf("%02d",m)
+        print(paste0("starting ncsv=",ncsv,",yyyy=",yyyy,",mm=",mm))
+        csv[[ncsv]]<-read_csv(file=paste0("https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_",yyyy,"-",mm,".csv"))
+        print(paste0("finished ncsv=",ncsv,",yyyy=",yyyy,",mm=",mm))
+      }
+      else {
+        print(paste0("skipping ncsv=",ncsv,",yyyy=",yyyy,",mm=",mm))
+      }
     }
   }
 }
@@ -31,5 +39,5 @@ ncsvs<-ncsv
 
 print(paste0("successfully read in ",ncsvs," csv files"))
 
-
+sink()
 
